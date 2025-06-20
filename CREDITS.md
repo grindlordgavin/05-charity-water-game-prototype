e71f60e330fa974ce0add4c33b88132cfcd2830c
Michael Campbell <me@micycle.dev>
2025-06-20 14:21:59 -0400

    jerry can movement

---
+++ CREDITS.md
@@ -0,0 +1,3 @@
+charity: water branding from charity: water
+
+water droplet from Twitter emoji set (twemoji) [link](https://www.svgrepo.com/svg/405249/droplet)
--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-// Log a message to the console to ensure the script is linked correctly
-console.log('JavaScript file is linked correctly.');
+const gameWorld = document.querySelector('.game-world');
+const jerry = document.querySelector('.jerry');
+const jerryRail = document.querySelector('.jerry-rail');
+
+const jerryBox = jerry.getBoundingClientRect();
+const jerryXOffset = jerryBox.left + (jerryBox.width / 2);
+
+const leftMax = -jerryBox.width / 2;
+const rightMax = jerryRail.clientWidth - jerryBox.width / 2;
+
+function moveJerryToX(xPos) {
+    xPos = Math.max(leftMax, xPos);
+    xPos = Math.min(rightMax, xPos);
+    // console.log('Mouse X:', xPos);
+    jerry.style.left = `${xPos}px`;
+}
+
+// attach event listener to the game world for mouse movement
+gameWorld.addEventListener('mousemove', (event) => {
+    moveJerryToX(event.clientX - jerryXOffset);
+});
+
+gameWorld.addEventListener('touchmove', (event) => {
+    event.preventDefault(); // Prevent default touch behavior
+    if (event.touches.length === 0) return; // No touches, exit early
+
+    const touch = event.touches[0];
+    moveJerryToX(touch.clientX - jerryXOffset);
+});
